50b86b4ba1c1e1c4deb1452a8a368e6a1098abcd
odalric2315 <odalrich78@gmail.com>
2020-12-18 17:16:03 +0700

    initial commit Minus

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,5 @@ fun main() {
     val nilai1: Double = readLine()?.toDouble()!!
     print("Masukkan nilai kedua : ")
     val nilai2: Double = readLine()?.toDouble()!!
+    println("Hasil Pengurangan dari perhitungan $nilai1 dan $nilai2 adalah = ${nilai1-nilai2}")
 }
