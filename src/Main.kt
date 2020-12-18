8fe76833ce8e5106e5512ebbd44a9ccb90e71ffb
odalric2315 <odalrich78@gmail.com>
2020-12-18 17:01:50 +0700

    initial commit

---
+++ src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    print("Masukkan nilai pertama : ")
+    val nilai1: Double = readLine()?.toDouble()!!
+    print("Masukkan nilai kedua : ")
+    val nilai2: Double = readLine()?.toDouble()!!
+}
